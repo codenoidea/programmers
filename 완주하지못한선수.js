1318ff514e41b5498df5195aa2749d2d200267b9
codenoidea <70764393+codenoidea@users.noreply.github.com>
2020-09-24 14:25:09 +0900

    Update 완주하지못한선수.js

--- 완주하지못한선수.js
+++ 완주하지못한선수.js
@@ -1,4 +1,4 @@
-// 처음생각한 
+// 처음생각한 - 정확성/효율성 실패
 function solution(participant, completion) {
     var answer = '';
     const duplicateName = participant.filter((item, pos) => {
@@ -13,5 +13,22 @@ function solution(participant, completion) {
       answer = value;
     };
     
+    return answer;
+}
+
+
+// 2번째 생각한 - 정확성 통과/효율성 실패
+function solution(participant, completion) {
+    var answer = '';
+    completion.forEach(x => {
+        const idx = participant.indexOf(x);
+      delete participant[idx]
+    })
+
+    for(let value of participant) {
+        if(value) {
+            answer = value  
+      }
+    }
     return answer;
 }
